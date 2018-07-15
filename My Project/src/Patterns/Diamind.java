2d3a9966168e70341e8bf62a567ae1ceb4051c19
meetparag81 <paragborawake81@gmail.com>
2018-07-15 23:18:36 +0530

    dimond pattern

--- My Project/src/Patterns/Diamind.java
+++ My Project/src/Patterns/Diamind.java
@@ -11,34 +11,38 @@ package Patterns;
 
 public class Diamind 
 {
+	
 
 	public static void main(String[] args) 
 	{
-		for(int i=1;i<=5;i++)
+		int i,j,k,l,m,n;
+		for( i=1;i<=5;i++)
 		{	
-			for(int k=3;k>=i;k--)
+			for( k=3;k>=i;k--)
 			{
 			System.out.print(" ");
 			}
-			for(int j=1;j<=i;j++)
+			for( j=1;j<=i;j++)
 			{
 				System.out.print("*" + " ");
 			}
 			System.out.println("");
+		}
 			
-			for(int l=1;l>5;l--)
+			for( l=4;l>0;l--)
 			{	
-				for(int m=3;m<=i;m++)
+				for( m=0;m>=l;m++)// for space starts from 0 and increased up to 10th row
 				{
 				System.out.print(" ");
 				}
-				for(int n=1;n>=l;n++)
+				for( n=1;n<=l;n++)
 				{
-					System.out.print("*" + " ");
+					System.out.print(" " +"*" );
 				}
 				System.out.println("");
 			}
-		}	
+			
+		
 
 	}
 	
