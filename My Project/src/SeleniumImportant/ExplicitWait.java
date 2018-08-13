b7084c3052e2850f38e90bec7a0a5a43fb95886a
meetparag81 <paragborawake81@gmail.com>
2018-08-14 00:00:22 +0530

    Small changes

--- My Project/src/SeleniumImportant/ExplicitWait.java
+++ My Project/src/SeleniumImportant/ExplicitWait.java
@@ -13,6 +13,7 @@ public class ExplicitWait
 		WebDriver driver = null;
 	WebDriverWait wait = new WebDriverWait(driver, 30, 100);
 	wait.until(ExpectedConditions.numberOfWindowsToBe(2));// this will be used upto the windows are not opened 
+	
 
 	}
 
