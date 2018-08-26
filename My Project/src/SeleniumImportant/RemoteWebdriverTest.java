6066a17fcca02366c1143f9be38a6bf665e1f0fc
meetparag81 <paragborawake81@gmail.com>
2018-08-26 23:00:37 +0530

    remote webdriver page

---
+++ My Project/src/SeleniumImportant/RemoteWebdriverTest.java
@@ -0,0 +1,14 @@
+package SeleniumImportant;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class RemoteWebdriverTest {
+
+	public static void main(String[] args) 
+	{
+		WebDriver driver = new RemoteWebDriver(desiredCapabilities)
+
+	}
+
+}
